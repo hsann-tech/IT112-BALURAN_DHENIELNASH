c924ac8a35ea4d6fcf33eb630a0a1974a225107d
Nash Baluran <nashybaluran@gmail.com>
2026-08-16 15:28:34 +0800

    Update LE1- BALURAN_DHENIEL NASH2.c

--- LE1- BALURAN_DHENIEL NASH2.c
+++ LE1- BALURAN_DHENIEL NASH2.c
@@ -3,10 +3,12 @@
 int main() {
     printf("************************************\n");
     printf("*                                  *\n");
-    printf("*       WELCOME TO C PROGRAMMING   *\n");
+    printf("*       COMPUTER PROGRAMMING 1     *\n");
     printf("*                                  *\n");
     printf("*          EAT - LEARN - CODE      *\n");
-    printf("*                                  *\n");
+    printf("*==================================*\n");
+    printf("|    CODE TODAY, CREATE TOMORROW!  |\n");
+    printf("*==================================*\n");
     printf("*                                  *\n");
     printf("************************************\n");
 
