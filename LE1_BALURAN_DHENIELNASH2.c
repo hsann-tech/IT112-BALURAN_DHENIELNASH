80c9c519dd1dc7f6e622bce86f1f23c61a9d0ba8
Nash Baluran <nashybaluran@gmail.com>
2026-08-16 15:41:05 +0800

    Add files via upload

---
+++ LE1_BALURAN_DHENIELNASH2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("|==================================|\n");
+    printf("|                                  |\n");
+    printf("|       COMPUTER PROGRAMMING 1     |\n");
+    printf("|                                  |\n");
+    printf("|         EAT - LEARN - CODE       |\n");
+    printf("|                                  |\n");
+    printf("|==================================|\n");
+    printf("|    CODE TODAY, CREATE TOMORROW!  |\n");
+    printf("|==================================|\n");
+    printf("|                                  |\n");
+    printf("|==================================|\n");
+
+    return 0;
+}
